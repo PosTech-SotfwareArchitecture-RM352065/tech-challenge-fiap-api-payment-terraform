6ee4039a42de2fa3e3b1e90a10220e4952931509
cangelosilima <cangelosilima@gmail.com>
2024-07-03 00:17:47 -0300

    ci: fix site varible name

--- github/main.tf
+++ github/main.tf
@@ -1,5 +1,5 @@
 resource "github_actions_organization_variable" "sanduba_payment_url" {
-  variable_name = "APP_COSTUMER_URL"
+  variable_name = "APP_PAYMENT_URL"
   visibility    = "all"
   value         = var.sanduba_payment_url
 }
